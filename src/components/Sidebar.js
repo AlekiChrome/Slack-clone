62db54fc66b5e5af305e1aa543243ca2189a9ad3
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-04-20 00:41:35 -0400

    Sidebar component created and imported, styling added to identify components in browser

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div style = {{background: "plum"}}>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar
